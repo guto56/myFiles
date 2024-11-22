a55592fee3b9b9ecf2d64672e46248609e5751d0
Seu Nome <alncorso@gmail.com>
2024-11-22 16:54:21 -0400

    criação da página de backup (não funcional)

--- arquivosImg/script.js
+++ arquivosImg/script.js
@@ -3,19 +3,28 @@ btn.addEventListener('click', function(){
     const content = document.querySelector('.content');
     const div = document.createElement('div');
     div.className = 'pasta';
+    div.id = 'contentDiv'
 
     const img = document.createElement('img');
     img.src = './img/pasta-removebg-preview.png';
 
     const title = document.createElement('a');
     title.innerText = prompt('Qual o nome do Arquivo?');
-    title.href = prompt('Diga o nome do arquivo caminho (exemplo.html).')
+    const titlePrompt = prompt('Diga o nome arquivo caminho. \n\nExemplo: teste \nResultado: teste.html')
+    title.href = titlePrompt + '.html';
 
     const desc = document.createElement('p');
-    desc.innerText = prompt('Qual a descrição de sua Página?')
+    desc.innerText = prompt('Qual a descrição de sua Página?');
     desc.id = 'desc';
 
-    div.append(img, title, desc)
-    content.append(div)
+    const removeBtn = document.createElement('button');
+    removeBtn.innerText = 'Remover';
+    removeBtn.id = 'removeBtn';
+    removeBtn.addEventListener('click', function(){
+        content.removeChild(div);
+    });
 
-})
+    div.append(img, title, desc, removeBtn);
+    content.append(div);
+
+});
