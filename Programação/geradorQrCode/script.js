5b41cfb77257d9488e94ffd4868eb598b24b82f1
Seu Nome <alncorso@gmail.com>
2024-12-27 16:52:23 -0400

    Página compressor + geradorQrCode

--- Programação/geradorQrCode/script.js
+++ Programação/geradorQrCode/script.js
@@ -1,4 +1,5 @@
 document.getElementById('generateBtn').addEventListener('click', function() {
+    
     const inputText = document.getElementById('inputText').value;
     const qrcodeImg = document.getElementById('qrcode');
 
@@ -18,11 +19,44 @@ document.getElementById('generateBtn').addEventListener('click', function() {
 
 const qrcodeImg = document.getElementById('qrcode');
 const title = document.getElementById('title');
-qrcodeImg.addEventListener('mouseover', function(){
+
+qrcodeImg.addEventListener('mouseover', function() {
     const inputText = document.getElementById('inputText').value;
-    title.innerText = `${inputText}`;
+    
+    // Fade out
+    title.style.opacity = '0'; // Torna o título invisível
+    
+    setTimeout(() => {
+        title.innerText = `${inputText}`; // Altera o texto
+        title.style.opacity = '1'; // Torna o título visível novamente
+    }, 300); // Tempo igual ao da transição
+});
+
+qrcodeImg.addEventListener('mouseleave', function() {
+    // Fade out
+    title.style.opacity = '0'; // Torna o título invisível
+    
+    setTimeout(() => {
+        title.innerText = 'Gerador de QR Code'; // Altera o texto
+        title.style.opacity = '1'; // Torna o título visível novamente
+    }, 300); // Tempo igual ao da transição
+});
+
+const inputText = document.getElementById('inputText');
+
+inputText.addEventListener('input', function() {
+    if (inputText.value.length > 1) {
+        inputText.style.width = '250px'; // Altera o tamanho para 200px
+    } else {
+        inputText.style.width = '10px'; // Retorna ao tamanho original
+    }
+});
+
+const img = document.getElementById('qrcode');
+img.addEventListener('mouseenter', function(){
+    img.style.transform = 'scale(1.1)';
 })
 
-qrcodeImg.addEventListener('mouseleave', function(){
-    title.innerText = 'Gerador de QR Code   ';
+img.addEventListener('mouseleave', function(){
+    img.style.transform = 'scale(1.0)';
 })
