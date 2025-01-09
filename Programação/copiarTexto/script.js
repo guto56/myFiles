a199085e62b09db06caf7631a5dcaa6dcfc23e75
Seu Nome <alncorso@gmail.com>
2025-01-09 14:22:25 -0400

    exclusão de algumas pastas

--- Programação/copiarTexto/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('copyBtn').addEventListener('click', function(){
-    const text = document.getElementById('texto');
-    text.select();
-
-    document.execCommand('copy')
-})
