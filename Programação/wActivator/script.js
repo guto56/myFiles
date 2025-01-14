20cb72a5b3f840f426bf34e8197e609069e94ef0
Seu Nome <alncorso@gmail.com>
2025-01-14 17:29:55 -0400

    generator

--- Programação/wActivator/script.js
+++ Programação/wActivator/script.js
@@ -1,13 +1,4 @@
 document.getElementById('generateBtn').addEventListener('click', function(){
-    const senha = document.getElementById('senha').value;
-
-    if(senha === '4886'){
-        alert('Senha correta!');
-        const senha = document.getElementById('senha');
-        senha.value = '';
-    }else{
-        alert('Senha incorreta!');
-        const senha = document.getElementById('senha');
-        senha.value = '';
-    }
+    const msg = document.getElementById('msg');
+    msg.innerText = 'teste';
 })
