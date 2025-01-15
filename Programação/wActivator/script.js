7f37982c0c80457e4e8812f5ea76ab1102d9271f
guto56 <alncorso@gmail.com>
2025-01-15 00:18:17 -0300

    wActivator

--- Programação/wActivator/script.js
+++ Programação/wActivator/script.js
@@ -1,4 +1,28 @@
-document.getEledqadqdqmentById('generateBtn').addEventListener('click', function(){
-    const msg = document.getElementById('msg');
-    msg.innerText = 'tedddte';
-})
+document.getElementById('generateBtn').addEventListener('click', function(){
+
+    const inputText = document.getElementById('key');
+
+    inputText.style.width = '150px'; // Altera
+
+    const div = document.getElementById('newBtnDiv');
+
+    const newBtn = document.createElement('button');
+    newBtn.innerText = 'Sim, ativar.';
+    newBtn.id = 'newBtnId';
+
+    div.append(newBtn);
+
+    newBtn.addEventListener('click', function(){
+        const inputText = document.getElementById('key');
+        inputText.style.width = '200px';
+        inputText.style.marginTop = '35px';
+        inputText.style.transform = 'scale(1.2)';
+        inputText.style.boxShadow = '0 0 15px rgba(25, 124, 194, 0.5)';
+
+        const reedem = document.querySelector('.reedem');
+        reedem.style.width = '300px';
+        const activateBtn = document.getElementById('generateBtn');
+        reedem.removeChild(activateBtn);
+    });
+
+});
