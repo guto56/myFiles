080fe82f92005c5408d009ccc14ed7d65c14a402
Seu Nome <alncorso@gmail.com>
2025-01-15 15:48:10 -0400

    wActivator

--- Programação/wActivator/script.js
+++ Programação/wActivator/script.js
@@ -1,36 +1,54 @@
 document.getElementById('generateBtn').addEventListener('click', function(){
 
-    const inputText = document.getElementById('key');
-
-    inputText.style.width = '150px'; // Altera
-
-    const div = document.getElementById('newBtnDiv');
-
-    const newBtn = document.createElement('button');
-    newBtn.innerText = 'Sim, ativar.';
-    newBtn.id = 'newBtnId';
-
-    div.append(newBtn);
-
-    newBtn.addEventListener('click', function(){
+    const inputText = document.getElementById('key').value;
+    if(inputText === 'teste'){
         const inputText = document.getElementById('key');
-        inputText.readOnly = true;
-        inputText.style.width = '200px';
-        inputText.style.marginTop = '35px';
-        inputText.style.transform = 'scale(1.2)';
-        inputText.style.boxShadow = '0 0 15px rgba(25, 124, 194, 0.5)';
-        inputText.classList.add('locked');
 
-        const reedem = document.querySelector('.reedem');
-        reedem.style.width = '300px';
-        const activateBtn = document.getElementById('generateBtn');
-        reedem.removeChild(activateBtn);
-
+        inputText.style.width = '150px'; // Altera
+    
         const div = document.getElementById('newBtnDiv');
-        inputText.style
-        div.removeChild(newBtn);
-        const msg = document.getElementById('msg');
-        
-    });
+    
+        const newBtn = document.createElement('button');
+        newBtn.innerText = 'Sim, ativar.';
+        newBtn.id = 'newBtnId';
+    
+        div.append(newBtn);
+    
+        newBtn.addEventListener('click', function(){
+            const inputText = document.getElementById('key');
+            inputText.readOnly = true;
+            inputText.style.width = '200px';
+            inputText.style.marginTop = '35px';
+            inputText.style.transform = 'scale(1.2)';
+            inputText.style.boxShadow = '0 0 15px rgba(25, 124, 194, 0.5)';
+            inputText.classList.add('locked');
+    
+            const reedem = document.querySelector('.reedem');
+            reedem.style.width = '300px';
+            const activateBtn = document.getElementById('generateBtn');
+            reedem.removeChild(activateBtn);
+    
+            const div = document.getElementById('newBtnDiv');
+            inputText.style
+            div.removeChild(newBtn);
+            
+            const howToGetKey = document.getElementById('howToGetKey');
+            const p = document.createElement('p');
+            const codigo = 'irm https://massgrave.dev/get | iex';
+            codigo.id = 'codId';
+            p.innerHTML = `Chave Utilizada: <strong>${inputText.value}</strong>
+            <br>1: Copie o código: <br><strong>${codigo}</strong>.
+            <br>2: Abra o Windows PowerShell (Administrador).
+            <br>3: Cole o Código e espere.
+            <br>4: Siga as instruções simples no Prompt de Comando (CMD).`;
+    
+            howToGetKey.append(p);
+            
+        });
+    }else{
+        alert('Chave inválida');
+    }
+
+   
 
 });
