3ad21c7cf6b1c12ee38a9c98ef86e7f003085a26
Seu Nome <alncorso@gmail.com>
2025-01-30 16:52:30 -0400

    desconsiderar

--- Programação/wActivator/comprarChave/script.js
+++ Programação/wActivator/comprarChave/script.js
@@ -1,7 +1,6 @@
-const buyBtn = document.getElementById('buyBtn');
-buyBtn.addEventListener('click', function(){
+function buyItem(){
     const phoneNumber = '5565981015757';
-    const message = 'Olá, tenho interesse em adquirir um produto.';
+    const message = 'Olá, tenho interesse em adquirir um produto do pacote.';
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.location.href = whatsappUrl;
-});
+}
