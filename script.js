fbb903d1d3712eb0ed845da2d8d5fc3fd047d841
Seu Nome <alncorso@gmail.com>
2025-02-17 16:07:43 -0400

    MAR Style and Photos

--- script.js
+++ script.js
@@ -19,3 +19,19 @@ addBtn.addEventListener('mouseout', function(){
     addBtn.style.transition = 'transform 0.5s ease';
     addBtn.style.transform = 'scale(1)';
 })
+
+let scrollTimeout;
+
+window.addEventListener('scroll', function() {
+    const blurSection = document.querySelector('.blur-effect');
+    
+    const scrollPosition = window.scrollY;
+    const blurAmount = Math.min(scrollPosition * 0.1, 1.5);
+    blurSection.style.filter = `blur(${blurAmount}px)`;
+
+    clearTimeout(scrollTimeout);
+
+    scrollTimeout = setTimeout(function() {
+        blurSection.style.filter = 'blur(0px)'; 
+    }, 30);
+});
