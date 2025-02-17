62e7d8d4ba6c6ea2221d4f95a0df62195d143311
Seu Nome <alncorso@gmail.com>
2025-02-17 16:46:07 -0400

    arquivo não utilizados = lixo

--- Programação/aula9.py
+++
@@ -1,7 +0,0 @@
-primeiro_valor = input('Digite o primeiro número: ')
-segundo_valor = input('Digite o segundo valor: ')
-
-if primeiro_valor > segundo_valor:
-    print(f'{primeiro_valor=} é maior que {segundo_valor}')
-else:
-    print(f'{segundo_valor=} é maior que {primeiro_valor}')
