c3e5da50949dca08f8acb238f3fe00f194cdd979
Seu Nome <alncorso@gmail.com>
2025-02-28 17:13:47 -0400

    aula93

---
+++ Python/aula93.py
@@ -0,0 +1,8 @@
+frase = 'Ola, como vc esta fazendo tanta coisa?'
+nova_lista = frase.split()
+
+for i, frase in enumerate(nova_lista):
+    print(nova_lista[i].strip())
+
+# print(frase)
+# print(nova_lista)
