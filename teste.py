9dd560666a888cf4e5e1e5465ea6fbd7b6cfdbc8
Seu Nome <alncorso@gmail.com>
2025-03-06 17:15:02 -0400

    teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,14 @@
-def soma(a, b):
-    print(a + b)
-    return a, b
+# def soma(a, b):
+#     print(a + b)
+#     return a, b
 
-soma(4, 3)
+# soma(4, 3)
+
+def mult(*args):
+    conta = 0
+    for numero in args:
+        conta = numero * numero
+    return conta
+
+mult(1, 1)
+print(mult)
