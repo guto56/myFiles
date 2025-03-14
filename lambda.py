886c59e35427dac2b2a8c34f25928521b93a642f
Seu Nome <alncorso@gmail.com>
2025-03-14 15:17:30 -0400

    teste = lambda. Function lambda

--- lambda.py
+++ lambda.py
@@ -15,9 +15,13 @@ lista = [
     {'nome': 'Aline', 'sobrenome': 'Souza'},
 ]
 
-def ordena(item):
-    print(item)
-    return item
+def exibir(lista):
+    for item in lista:
+        print(item)
+    print()
 
-for dict in lista:
-    print(dict)
+l1 = sorted(lista, key=lambda item: item['nome'])
+l2 = sorted(lista, key=lambda item: item['sobrenome'])
+
+exibir(l1)
+exibir(l2)
