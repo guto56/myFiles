21a96c3c4d43c849726d7f02d2afa52d8e89b459
Augusto Luiz <alncorso@gmail.com>
2025-03-14 11:57:05 -0400

    teste.py

---
+++ teste.py
@@ -0,0 +1,16 @@
+import os
+os.system('clear')
+
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+def ordena(item):
+    print(item)
+    return item
+
+print(*lista)
