846efbba19b7503189fde91590638455b0ecebfa
Augusto Luiz <alncorso@gmail.com>
2025-03-14 19:55:04 -0400

    teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,25 @@
 import os
 
 if os.name == 'nt':
-    print('windows')
+    os.system('cls')
 else:
-    print('mac os')˝
+    os.system('clear')
+
+lista = []
+
+while True:
+    opc = input('O que deseja fazer? A[Adicionar] / R[Remover] / L[Listar]: ').lower()
+
+    if opc == 'a':
+        adicionar = input('O que deseja adicionar? ')
+        lista.append(adicionar)
+    elif opc == 'r':
+        remover = input('Digite o índice que deseja remover: ')
+        try:
+            
+        continue
+    elif opc == 'l':
+        for i, item in enumerate(lista):
+            print(i, ':', item)
+    else:
+        print('Selecione a opção correta.')
