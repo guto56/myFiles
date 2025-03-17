359afa0a3e13f413879dafa4ef2d353fa56f7364
guto56 <alncorso@gmail.com>
2025-03-17 12:24:16 -0400

    teste.py

--- teste.py
+++ teste.py
@@ -1,33 +1,11 @@
 import os
+import pprint
 
 if os.name == 'nt':
-    os.system('cls')
+     os.system('cls')
 else:
-    os.system('clear')
+     os.system('clear')
 
-lista = []
+linhas_colunas = [(x, y) for x in range(11) for y in range(7)]
 
-while True:
-    opc = input('O que deseja fazer? A[Adicionar] / R[Remover] / L[Listar]: ').lower()
-
-    if opc == 'a':
-        adicionar = input('O que deseja adicionar? ')
-        lista.append(adicionar)
-    elif opc == 'r':
-        try:
-            if len(lista) < 1:
-                print('Nada para remover')
-                continue
-            remover = input('Digite o índice que deseja remover: ')
-            remover_i = int(remover)
-            del lista[remover_i]
-            print('Removido.')
-        except:
-            print('Erro')
-    elif opc == 'l':
-        if len(lista) < 1:
-            print('Nada para listar.')
-        for i, item in enumerate(lista):
-            print(i, ':', item)
-    else:
-        print('Selecione a opção correta.')
+print(linhas_colunas)˝
