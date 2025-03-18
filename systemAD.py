c40fb35735c474e9f8fb104fb465b894de588b88
guto56 <alncorso@gmail.com>
2025-03-18 16:53:46 -0400

    system>

--- systemAD.py
+++ systemAD.py
@@ -1,3 +1,4 @@
+#made by augusto.
 import os
 
 if os.name == 'nt':
@@ -5,3 +6,19 @@ if os.name == 'nt':
 else:
     os.system('clear')  # Limpa a tela no Linux/Mac
 
+lista = []
+
+while True:
+    opc = input('Digite de 1 até 4: ')
+
+    if opc == '1':
+        print('Opc 1')
+    elif opc == '2':
+        print('Opc 2')
+    elif opc == '3':
+        print('Opc 3')
+    elif opc == '4':
+        break
+    else:
+        print("Opc invalida")
+        continue
