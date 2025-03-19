79defd3654a39debc0c01246f62d69780154d97a
Seu Nome <alncorso@gmail.com>
2025-03-19 15:33:05 -0400

    iterator and generator .py

---
+++ Python/iteratorAndGenerator.py
@@ -0,0 +1,22 @@
+#made by augusto.
+import os
+import sys
+
+if os.name == 'nt':
+    os.system('cls')  # Limpa a tela no Windows
+else:
+    os.system('clear')  # Limpa a tela no Linux/Mac
+
+# Generator expression, Iterables e Iterators em Python
+iterable = ['Eu', 'Tenho', '__iter__']
+iterator = iter(iterable)  # tem __iter__ e __next__
+lista = [n for n in range(1000000)]
+generator = (n for n in range(1000000))
+
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
+
+print(generator)
+
+# for n in generator:
+#     print(n)
